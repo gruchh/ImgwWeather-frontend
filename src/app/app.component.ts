d8fb1fc86f57d24c2addb600bec25a65d7562068
Kuba <kubol2@gmail.com>
2023-02-09 09:10:19 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather-frontend';
+
+constructor(private appService: AppService) { }
+
+login() {
+  let payload = {email: this.email, password: this.password}
+  this.appService.logInApi(payload).subscribe( (res : any) => {
+    this.getData(res.token)
+  });
+
+}
+
+getData(token: string)
+{
+  this.appService.getData(token).subscribe((res: Data[]) => {
+    console.log(res);
+    this.data = res;
+  })
+}
+
+}
+
+export interface Data {
+id: number;
+measurementValue: any;
+measureDate: Data;
 }
